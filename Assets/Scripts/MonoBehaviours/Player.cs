d38825f556cccd4deb330d87945f1b74f749d492
Vaclav Hnizda <vaclav.cz@gmail.com>
2020-04-16 20:33:35 +0200

    coin collected logic in place

--- Assets/Scripts/MonoBehaviours/Player.cs
+++ Assets/Scripts/MonoBehaviours/Player.cs
@@ -4,5 +4,11 @@ using UnityEngine;
 
 public class Player : Character
 {
-    
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("CanBePickedUp"))
+        {
+            collision.gameObject.SetActive(false);
+        }
+    }
 }
