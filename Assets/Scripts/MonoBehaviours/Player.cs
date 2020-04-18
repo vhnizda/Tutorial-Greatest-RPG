f5c9a328ad8e4cbe7574388d1e3fe6aafca43c37
Vaclav Hnizda <vaclav.cz@gmail.com>
2020-04-18 13:43:29 +0200

    chapter 5 complete

--- Assets/Scripts/MonoBehaviours/Player.cs
+++ Assets/Scripts/MonoBehaviours/Player.cs
@@ -13,8 +13,27 @@ public class Player : Character
             if (hitObject != null)
             {
                 print("Hit: " + hitObject.objectName);
+
+                switch (hitObject.itemType)
+                {
+                    case Item.ItemType.COIN:
+                        break;
+
+                    case Item.ItemType.HEALTH:
+                        AdjustHitPoints(hitObject.quantity);
+                        break;
+                    default:
+                        break;
+                }
+
                 collision.gameObject.SetActive(false);
             }
         }
+    }
+
+    public void AdjustHitPoints(int amount)
+    {
+        hitPoints = hitPoints + amount;
+        print("Adjusted hitpoints by: " + amount + ". New value: " + hitPoints);
     }
 }
