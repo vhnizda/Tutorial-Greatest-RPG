159ee6c42a1afe5f25890b664bde51ed9d89e189
Vaclav Hnizda <vaclav.cz@gmail.com>
2020-04-20 18:45:18 +0200

    health bar - but with Bug - null pointer exception

--- Assets/Scripts/MonoBehaviours/Player.cs
+++ Assets/Scripts/MonoBehaviours/Player.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class Player : Character
 {
+    public HealthBar healthBarPrefab;
+    HealthBar healthBar;
+
+    public void Start()
+    {
+        hitPoints.value = startingHitPoints;
+        healthBar = Instantiate(healthBarPrefab);
+        healthBar.character = this;
+    }
+
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("CanBePickedUp"))
@@ -12,28 +22,35 @@ public class Player : Character
 
             if (hitObject != null)
             {
-                print("Hit: " + hitObject.objectName);
-
+                bool shouldDisappear = false;
                 switch (hitObject.itemType)
                 {
                     case Item.ItemType.COIN:
+                        shouldDisappear = true;
                         break;
 
                     case Item.ItemType.HEALTH:
-                        AdjustHitPoints(hitObject.quantity);
+                        shouldDisappear = AdjustHitPoints(hitObject.quantity);
                         break;
                     default:
                         break;
                 }
-
-                collision.gameObject.SetActive(false);
+                if (shouldDisappear)
+                {
+                    collision.gameObject.SetActive(false);
+                }
             }
         }
     }
 
-    public void AdjustHitPoints(int amount)
+    public bool AdjustHitPoints(int amount)
     {
-        hitPoints = hitPoints + amount;
-        print("Adjusted hitpoints by: " + amount + ". New value: " + hitPoints);
+        if (hitPoints.value < maxHitPoints)
+        {
+            hitPoints.value = hitPoints.value + amount;
+            print("Adjusted hitpoints by: " + amount + ". New value: " + hitPoints);
+            return true;
+        }
+        return false;
     }
 }
