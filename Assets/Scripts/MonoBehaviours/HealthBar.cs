cc6d172d1da89374317fe98e1a2625825a109bc9
Vaclav Hnizda <vaclav.cz@gmail.com>
2020-04-21 20:16:33 +0200

    fix null error
    In the healthBar start() was being called twice, I needed to put in a check for null., so that it's ignored the first time and then everything runs fine.

--- Assets/Scripts/MonoBehaviours/HealthBar.cs
+++ Assets/Scripts/MonoBehaviours/HealthBar.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
@@ -15,7 +13,10 @@ public class HealthBar : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        maxHitPoints = character.maxHitPoints;
+        if (character != null)
+        {
+            maxHitPoints = character.maxHitPoints;
+        }
     }
 
     // Update is called once per frame
